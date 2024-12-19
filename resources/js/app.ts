de59fdc720ae05fae211b91ad01f3c51478f9a48
xbetozar <monrazdot@gmail.com>
2024-12-19 15:37:27 -0600

    AlpineJS added

--- resources/js/app.ts
+++ resources/js/app.ts
@@ -1,9 +1,21 @@
+import Alpine from 'alpinejs'
+
+declare global {
+  interface Window {
+    Alpine: object,
+  }
+}
+
+window.Alpine = Alpine
+
+Alpine.start()
+
 class Greeting {
   static sayHello(): string {
-    return 'Hi!';
+    return 'Hi!'
   }
 }
 
 document.addEventListener('DOMContentLoaded', () => {
-  document.body.innerHTML += `<p class="text-3xl text-gray-400">${Greeting.sayHello()}</p>`;
-});
+  document.body.innerHTML += `<p class="text-3xl text-gray-400">${Greeting.sayHello()}</p>`
+})
