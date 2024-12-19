9f5b1ea48b33ccdd31e50c6593d0da34eea41547
xbetozar <monrazdot@gmail.com>
2024-12-19 14:17:41 -0600

    tailwind added

--- resources/js/app.ts
+++ resources/js/app.ts
@@ -4,4 +4,6 @@ class Greeting {
   }
 }
 
-console.log(Greeting.sayHello());
+document.addEventListener('DOMContentLoaded', () => {
+  document.body.innerHTML += `<p class="text-3xl text-gray-400">${Greeting.sayHello()}</p>`;
+});
---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./resources/**/*.{edge,ts}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
